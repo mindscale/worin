071afa2f866087dd7ab3d5538bd5a34c7ed689da
Jae-Myoung Yu <euphoris@gmail.com>
2018-04-12 16:47:08 +0900

    fix package data

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='worin',
-      version='0.0.1',
+      version='0.0.2',
       description='A Korean POS Tagger using Neural Network',
       author='YU Jaemyoung',
       author_email='yu@mindscale.kr',
       url='https://github.com/mindscale/worin',
       packages=['worin'],
+      package_data={
+          '': ['*.txt', '*.md'],
+          'worin': ['*.h5'],
+      },
       install_requires=[
           'keras',
           'h5py',
